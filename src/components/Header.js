dd75421a515f6328809d64f4e0c51042414946fc
Lucas <lucas2carlos@gmail.com>
2022-07-04 18:10:16 -0400

    building Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
+import {BsLinkedin} from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+
 export default function Headers(props) {
     return (
-        <div className="Header">This is the Header</div>
+        <div className="Header">
+            <div id="logo"></div>
+            <ul>
+                <li><a href='https://github.com/lucas9121' target='_blank'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
+                <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank'> <BsLinkedin style={{height: '2rem', width: '3rem'}}/></a></li>
+            </ul>
+        </div>
     )
 }
