4a42a4046a7825ad486ffb8f64bcb201b2f903a8
Lucas <lucas2carlos@gmail.com>
2022-07-06 18:07:37 -0400

    adding github

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Headers(props) {
         <div className="Header">
             <div id="logo"></div>
             <ul>
-                <li><a href='https://github.com/lucas9121' target='_blank' className='Link' id='Github'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
+                <li><a href='https://github.com/lucas9121' target='_blank' className='Link Github'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
                 <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank' className='Link' id='Linkedin'> <BsLinkedin style={{height: '2rem', width: '3rem'}}/></a></li>
             </ul>
         </div>
