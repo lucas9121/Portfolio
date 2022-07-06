cbf244bbb3d6412b6cde5e87bf82dd74a53e7d9b
Lucas <lucas2carlos@gmail.com>
2022-07-06 17:48:43 -0400

    adding unit 3 project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,12 +8,12 @@ export default function Projects(props) {
     return (
         <div id="Projects">
             <h2>My Projects</h2>
-            <article class="my-projects">
+            <article className="my-projects">
                 <div id="GH-picture"></div>
-                <div class="game-description">
+                <div className="game-description">
                     <h1>Gamehub</h1>
                     <p><span id="GH">G</span>amehub is a full CRUD mock online gaming store. You can browse through different game titles or search for them in the search bar. Some of the features of available include; purchasing games, writing reviews, as well as editing or deleting your reviews, creating and editing your own games and editing your account. Sign up with a Gamer or Developer account to try all of the different features the website has to offer.</p>
-                    <div class="tech">
+                    <div className="tech">
                         <div>
                             <p>HTML</p>
                             <AiOutlineHtml5 style={{height: '2rem', width: '3rem'}} />
@@ -30,7 +30,7 @@ export default function Projects(props) {
                             <p>MongoDB</p>
                             <SiMongodb style={{height: '2rem', width: '3rem'}}/>
                         </div>
-                        <div style="height: 30px; width: 30px;">
+                        <div>
                             <p>ExpressJs</p>
                             <SiExpress style={{height: '2rem', width: '3rem'}}/>
                         </div>
@@ -42,14 +42,14 @@ export default function Projects(props) {
                             <p>NodeJS</p>
                             <IoLogoNodejs style={{height: '2rem', width: '3rem'}}/>
                         </div>
-                        <div style="height: 30px; width: 30px;">
+                        <div>
                             <p>Heroku</p>
                             <SiHeroku style={{height: '2rem', width: '3rem'}}/>
                         </div>
                     </div>
                     <ul>
-                        <li><a class="Link" href="https://github.com/lucas9121/Gamehub" target="_blank"><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
-                        <li><a class="Link" href="https://gamehub-react.herokuapp.com/games" target="_blank"><BsJoystick style={{height: '2rem', width: '3rem'}} /></a></li>
+                        <li><a className="Link" href="https://github.com/lucas9121/Gamehub" target="_blank"><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
+                        <li><a className="Link" href="https://gamehub-react.herokuapp.com/games" target="_blank"><BsJoystick style={{height: '2rem', width: '3rem'}} /></a></li>
                     </ul>
                 </div>
             </article>
