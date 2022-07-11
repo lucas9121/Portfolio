b7f7998461a7618f956780e2d0a095af20c3962b
Lucas <lucas2carlos@gmail.com>
2022-07-11 19:16:09 -0400

    adding Roaming spoon carousel images

---
+++ src/components/RFTImages.js
@@ -0,0 +1,29 @@
+const RFTImages = [
+    {
+        id: 1,
+        src: "/images/RFT/RFT1.png",
+        alt: "Roaming Spoon image 1"
+    },
+    {
+        id: 2,
+        src: "/images/RFT/RFT2.png",
+        alt: "Roaming Spoon image 2"
+    },
+    {
+        id: 3,
+        src: "/images/RFT/RFT3.png",
+        alt: "Roaming Spoon image 3"
+    },
+    {
+        id: 4,
+        src: "/images/RFT/RFT4.png",
+        alt: "Roaming Spoon image 4"
+    },
+    {
+        id: 5,
+        src: "/images/RFT/RFT5.png",
+        alt: "Roaming Spoon image 5"
+    }
+]
+
+export default RFTImages
