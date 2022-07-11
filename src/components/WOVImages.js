c7448502d652a55e195db90461a4dc956ce4a10b
Lucas <lucas2carlos@gmail.com>
2022-07-11 19:16:40 -0400

    adding Wars of Vogrora carousel images

---
+++ src/components/WOVImages.js
@@ -0,0 +1,29 @@
+const WOVImages = [
+    {
+        id: 1,
+        src: "/images/WOV/WOV1.png",
+        alt: "Wars of Vogrora image 1"
+    },
+    {
+        id: 2,
+        src: "/images/WOV/WOV2.png",
+        alt: "Wars of Vogrora image 2"
+    },
+    {
+        id: 3,
+        src: "/images/WOV/WOV3.png",
+        alt: "Wars of Vogrora image 3"
+    },
+    {
+        id: 4,
+        src: "/images/WOV/WOV4.png",
+        alt: "Wars of Vogrora image 4"
+    },
+    {
+        id: 5,
+        src: "/images/WOV/WOV5.png",
+        alt: "Wars of Vogrora image 5"
+    }
+]
+
+export default WOVImages
