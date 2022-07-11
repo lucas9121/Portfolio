91067fe4f036cc08c7ed2cd8b6993b6dba675de5
Lucas <lucas2carlos@gmail.com>
2022-07-10 21:59:36 -0400

    adding toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
-import {BsLinkedin} from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
+import {BsLinkedin, BsGithub, BsSunFill, BsMoonFill} from 'react-icons/bs'
 
 export default function Headers(props) {
     return (
@@ -8,6 +7,16 @@ export default function Headers(props) {
             <ul>
                 <li><a href='https://github.com/lucas9121' target='_blank' className='Link Github'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
                 <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank' className='Link' id='Linkedin'> <BsLinkedin style={{height: '2rem', width: '3rem'}}/></a></li>
+                <li style={{height: '100%'}}>
+                    <div id="toggle">
+                        <BsSunFill style={{color: 'gold'}} />
+                        <label class="switch">
+                            <input type="checkbox"/>
+                            <span class="slider round"></span>
+                        </label>
+                        <BsMoonFill style={{color: '#2196F3'}} />
+                    </div>
+                </li>
             </ul>
         </div>
     )
