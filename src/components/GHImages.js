5917ca3063ba337a0885059eb05bcd930d54dd52
Lucas <lucas2carlos@gmail.com>
2022-07-11 19:15:46 -0400

    adding Gamehub carousel images

---
+++ src/components/GHImages.js
@@ -0,0 +1,38 @@
+const GHImages = [{
+    id: 1,
+    src: "/images/GH/GH1.png",
+    alt: "Gamehub image 1"
+},
+{
+    id: 2,
+    src: "/images/GH/GH2.png",
+    alt: "Gamehub image 2"
+},
+{
+    id: 3,
+    src: "/images/GH/GH3.png",
+    alt: "Gamehub image 3"
+},
+{
+    id: 4,
+    src: "/images/GH/GH4.png",
+    alt: "Gamehub image 4"
+},
+{
+    id: 5,
+    src: "/images/GH/GH5.png",
+    alt: "Gamehub image 5"
+},
+{
+    id: 6,
+    src: "/images/GH/GH6.png",
+    alt: "Gamehub image 6"
+},
+{
+    id: 7,
+    src: "/images/GH/GH7.png",
+    alt: "Gamehub image 7"
+}
+]
+
+export default GHImages
