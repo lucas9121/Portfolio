aab95e8a71f274b05ba31fb08e6928cd1e439b3f
Lucas <lucas2carlos@gmail.com>
2022-07-10 23:16:26 -0400

    adding onClick and props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import {BsLinkedin, BsGithub, BsSunFill, BsMoonFill} from 'react-icons/bs'
 
-export default function Headers(props) {
+export default function Headers({toggle, setToggle, bckgd}) {
     return (
-        <div className="Header">
+        <div className="Header" style={{background: `${bckgd}`}}>
             <div id="logo"></div>
             <ul>
                 <li><a href='https://github.com/lucas9121' target='_blank' className='Link Github'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
                 <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank' className='Link' id='Linkedin'> <BsLinkedin style={{height: '2rem', width: '3rem'}}/></a></li>
                 <li style={{height: '100%'}}>
                     <div id="toggle">
-                        <BsSunFill style={{color: 'gold'}} />
+                        <BsSunFill style={{color: 'yellow'}} />
                         <label class="switch">
-                            <input type="checkbox"/>
+                            <input type="checkbox" onClick={() => {setToggle(!toggle)}} />
                             <span class="slider round"></span>
                         </label>
                         <BsMoonFill style={{color: '#2196F3'}} />
