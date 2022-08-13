c03d63fcc209993a275ae0d2a7d116568ed4e2e3
Lucas <lucas2carlos@gmail.com>
2022-08-13 17:43:43 -0400

    adding responsive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,30 @@
 import {BsLinkedin, BsGithub, BsSunFill, BsMoonFill} from 'react-icons/bs'
 import "./Header.css"
 
-export default function Headers({toggle, setToggle, bckgd}) {
+export default function Headers({toggle, setToggle, bckgd, sticky}) {
     return (
-        <div className="Header" style={{background: `${bckgd}`}}>
-            <div id="logo"></div>
-            <ul>
-                <li><a href='https://github.com/lucas9121' target='_blank' className='Link Github'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
-                <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank' className='Link' id='Linkedin'> <BsLinkedin style={{height: '2rem', width: '3rem'}}/></a></li>
-                <li style={{height: '100%'}}>
-                    <div id="toggle">
-                        <BsSunFill style={{color: 'yellow'}} />
-                        <label className="switch">
-                            <input type="checkbox" onClick={() => {setToggle(!toggle)}} />
-                            <span className="slider round"></span>
-                        </label>
-                        <BsMoonFill style={{color: '#2196F3'}} />
-                    </div>
-                </li>
-            </ul>
+        <div className={sticky ? "Header sticky" : "Header"} style={{background: `${bckgd}`}}>
+            <img src="https://www.incimages.com/uploaded_files/image/1024x576/software-computer-code-1940x900_35196.jpg" alt="banner" id="banner" />
+            <a href='#' id="logo">Brand</a>
+            <nav>
+                <ul>
+                    <li className='list'><a href="#About">About</a></li>
+                    <li className='list'><a href="#Projects">Projects</a></li>
+                    <li className='list'><a href="mailto:lucas2carlos@gmail.com" target="_blank">Contact me</a></li>
+                    {/* <li><a href='https://github.com/lucas9121' target='_blank' className='Link Github'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
+                    <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank' className='Link' id='Linkedin'> <BsLinkedin style={{height: '2rem', width: '3rem'}}/></a></li> */}
+                    <li className='list' style={{height: '100%'}}>
+                        <div id="toggle">
+                            <BsSunFill style={{color: 'yellow'}} />
+                            <label className="switch">
+                                <input type="checkbox" onClick={() => {setToggle(!toggle)}} />
+                                <span className="slider round"></span>
+                            </label>
+                            <BsMoonFill style={{color: '#2196F3'}} />
+                        </div>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
