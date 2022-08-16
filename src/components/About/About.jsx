94cfb753dd90c52d595c5bf89ddc386f68ade121
Lucas <lucas2carlos@gmail.com>
2022-08-15 23:48:43 -0400

    adding about me section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,32 @@
+import './About.css'
+
+export default function About(){
+    return(
+        <article id="AboutMe">
+            <div id="About-img"></div>
+            <div>
+                <div id="About-description">
+                    <p>I'm a software engineer with background experience in both the criminal justice, and education field. I admire the problem-solving aspect, and versatility to create virtually anything, that the software engineer field offers. I enjoy building web apps and content that has a lot of functionality to it. </p>
+                    <p>During my free time I enjoy playing the piano or playing video games online and volunteering.</p>
+                    <ul id="skills">
+                        <li>Skills: </li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>JSX</li>
+                        <li>Github</li>
+                        <li>MongoDb</li>
+                        <li>ExpressJs</li>
+                        <li>React</li>
+                        <li>NodeJs</li>
+                        <li>MVC</li>
+                        <li>Postman</li>
+                        <li>Bootstrap</li>
+                        <li>Heroku</li>
+                        <li>Python</li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+    )
+}
