0f9a26654ba3524e6c72bfc83d7804441d54a342
Lucas <lucas2carlos@gmail.com>
2022-09-06 23:19:26 -0400

    adding use State and ternary

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
-import { BsSunFill, BsMoonFill, BsArrowDownCircle} from 'react-icons/bs'
+import { BsSunFill, BsMoonFill, BsArrowDownCircle } from 'react-icons/bs'
+import { BiMenu } from 'react-icons/bi'
 import "./Header.css"
+import { useState } from 'react'
 
 export default function Headers({toggle, setToggle, sticky, setSticky}) {
+    const [show, setShow] = useState(false)
+
     return (
         <div className={sticky ? "Header sticky" : "Header"}>
             <button id="logo" onClick={() => {setSticky(!sticky)}}></button>
             <BsArrowDownCircle id='arrow' onClick={() => {setSticky(!sticky)}} />
-            <nav>
+            <BiMenu className='Hamburger' onClick={() => {setShow(!show)}} />
+            <nav className={show ? "navigation" : ""}>
                 <ul>
                     <li className='list'><a className={toggle ? 'night' : ""} href="#About">About</a></li>
                     <li className='list'><a className={toggle ? 'night' : ""} href="#Projects">Projects</a></li>
