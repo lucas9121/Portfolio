924cb2424e402d8881629f3e95b1b86e71793825
Lucas <lucas2carlos@gmail.com>
2022-12-09 18:30:49 -0500

    changing link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(props) {
             <p>&copy;LC 2022</p>
             <nav>
                 <ul>
-                    <li><a href="https://drive.google.com/file/d/1YpJlHjD4-xLiO2jEqMzHzShInV6-21Mn/view?usp=sharing" target="_blank" download="resume" className="Link footerLink"><AiOutlineFileText style={{height: '2rem', width: '3rem'}}/></a></li>
+                    <li><a href="https://drive.google.com/file/d/1V9aUk79pMPNuxODgle9JL5Gwcd55XXKT/view?usp=share_link" target="_blank" download="resume" className="Link footerLink"><AiOutlineFileText style={{height: '2rem', width: '3rem'}}/></a></li>
                     <li><a href='https://github.com/lucas9121' target='_blank' className='Link Github footerLink'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
                     <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank' className='Link' id='Linkedin'> <BsLinkedin style={{height: '2rem', width: '3rem'}}/></a></li>
                 </ul>
