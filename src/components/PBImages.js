f5509fbd8a8768c693a0ae2b4425ab89510bfdf1
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-08 19:51:24 -0400

    fixing images

--- src/components/PBImages.js
+++ src/components/PBImages.js
@@ -1,23 +1,24 @@
-const PBImages = [{
-    id: 1,
-    src: "/images/PB/PB1.png",
-    alt: "Psych bite image 1"
-},
-{
-    id: 2,
-    src: "/images/PB/PB2.png",
-    alt: "Psych bite image 2"
-},
-{
-    id: 3,
-    src: "/images/PB/PB3.png",
-    alt: "Psych bite image 3"
-},
-{
-    id: 4,
-    src: "/images/PB/PB4.png",
-    alt: "Psych bite image 4"
-}
+const PBImages = [
+    {
+        id: 1,
+        src: "/images/PB/PB1.png",
+        alt: "Psych bite image 1"
+    },
+    {
+        id: 2,
+        src: "/images/PB/PB2.png",
+        alt: "Psych bite image 2"
+    },
+    {
+        id: 3,
+        src: "/images/PB/PB3.png",
+        alt: "Psych bite image 3"
+    },
+    {
+        id: 4,
+        src: "/images/PB/PB4.png",
+        alt: "Psych bite image 4"
+    }
 ]
 
 export default PBImages
