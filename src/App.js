742e60315606c3281a36d63d9aac0dd6107b7817
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-09 01:12:54 -0400

    updating code

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import "./App.css"
+import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import { useState, useEffect } from "react";
 
 export default function App() {
-  const [toggle, setToggle] = useState(false)
-  const [sticky, setSticky] = useState(false)
-  const [mode, setMode] = useState('')
+  const [toggle, setToggle] = useState(false);
+  const [sticky, setSticky] = useState(false);
+
 
   useEffect(() => {
-    if(toggle){
-      setMode('night mode')
-    } else if(mode !== '') {
-      setMode('day mode')
-    }
-    if(mode === 'day mode'){
-      document.querySelector(".subDiv").classList.add('dayMode')
-    } else {
-      document.querySelector(".subDiv").classList.remove('dayMode')
-    }
-  }, [toggle, mode])
-
-  useEffect(() => {
-    if(!sticky) document.querySelector(".subDiv").scrollTo(0, 0); document.querySelector(".ProjectsGrid").scrollTo(0, 0)
-  }, [sticky])
-
+    if (!sticky) document.querySelector(".content-section").scrollTo(0, 0);
+    document.querySelector(".ProjectsGrid").scrollTo(0, 0);
+  }, [sticky]);
 
   const handleScroll = (evt) => {
-    const scrollTop = evt.currentTarget.scrollTop
-    if(scrollTop > 0){
-      setSticky(true)
+    const scrollTop = evt.currentTarget.scrollTop;
+    if (scrollTop > 0) {
+      setSticky(true);
     } else {
-      setSticky(false)
+      setSticky(false);
     }
+  };
+
+  const handleScrollToTop = () => {
+    document.querySelector(".content-section").scrollTo(0, 0)
   }
 
   return (
-    <main className={toggle ? "App AppNight" : "App"} onScroll={(evt) => {handleScroll(evt)}}>
-        <Header toggle={toggle} setToggle={setToggle} sticky={sticky} setSticky={setSticky} setMode={setMode} mode={mode} />
-        <div className={toggle ? "subDiv subDivNight" : "subDiv"}>
-          <div className="Sun"></div>
-          <div className="Moon"></div>
+    <div
+      className={toggle ? "App AppNight" : "App"}
+      onScroll={(evt) => {
+        handleScroll(evt);
+      }}
+    >
+      <Header
+        toggle={toggle}
+        setToggle={setToggle}
+        sticky={sticky}
+        setSticky={setSticky}
+        handleScrollToTop={handleScrollToTop}
+      />
+      <section className={toggle ? "content-section content-section-dark" : "content-section"}>
+        <div className="background-layer"></div>
           <Main toggle={toggle} />
           <Footer toggle={toggle} />
-        </div>
-    </main>
-  )
+
+      </section>
+    </div>
+  );
 }
