b02a04eaf8139e77c9e8601159891f0b0d86c6ac
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-09 01:15:52 -0400

    adding function and id

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,31 +3,31 @@ import { BiMenu } from 'react-icons/bi'
 import "./Header.css"
 import { useState } from 'react'
 
-export default function Headers({toggle, setToggle, sticky, setSticky}) {
+export default function Headers({toggle, setToggle, sticky, setSticky, handleScrollToTop}) {
     const [show, setShow] = useState(false)
 
     return (
-        <div className={sticky ? "Header sticky" : "Header"}>
-            <button id="logo" onClick={() => {setSticky(!sticky); setShow(false)}}></button>
+        <header className={sticky ? "Header sticky" : "Header"}>
+            <div id={!sticky ? "logo" : "portrait"} onClick={() => handleScrollToTop()}></div>
             <BsArrowDownCircle id='arrow' onClick={() => {setSticky(!sticky)}} />
             <nav>
                 <BiMenu className='hamburger' onClick={() => {setShow(!show)}} />
                 <ul className={show ? "menu" : ""}>
                     <li className='list'><a className={toggle ? 'night' : ""} href="#About" onClick={() => {setShow(false)}}>About</a></li>
                     <li className='list'><a className={toggle ? 'night' : ""} href="#Projects" onClick={() => {setShow(false)}}>Projects</a></li>
-                    <li className='list'><a className={toggle ? 'night' : ""} href="mailto:lucas2carlos@gmail.com" target="_blank" onClick={() => {setShow(false)}}>Contact me</a></li>
+                    <li className='list'><a className={toggle ? 'night' : ""} href="mailto:lucas2carlos@gmail.com" target="_blank" rel="noreferrer" onClick={() => {setShow(false)}}>Contact me</a></li>
                     <li className='list' style={{height: '100%'}}>
                         <div id="toggle">
-                            <BsSunFill style={{color: 'yellow'}} />
+                            {/* <BsSunFill style={{color: 'yellow'}} /> */}
                             <label className="switch" onClick={() => {setShow(false)}}>
                                 <input type="checkbox" onClick={() => {setToggle(!toggle)}} />
                                 <span className="slider round"></span>
                             </label>
-                            <BsMoonFill style={{color: '#2196F3'}} />
+                            {/* <BsMoonFill style={{color: '#2196F3'}} /> */}
                         </div>
                     </li>
                 </ul>
             </nav>
-        </div>
+        </header>
     )
 }
