51150c229679b1d1fa8336e8874ca8ae5af307b2
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-10 02:35:52 -0400

    cleaning code

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,6 @@ export default function Main({ toggle }) {
   return (
     <main className={toggle ? "Main MainNight" : "Main"}>
       <About toggle={toggle} />
-      <h2 id="Projects">My Projects</h2>
       <Projects toggle={toggle} />
     </main>
   );
