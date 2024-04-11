2a4e7f29ad4a807aca6e5f8da2c8ba7ef97a618a
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-10 22:40:19 -0400

    optimizing images

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -14,13 +14,21 @@ export default function ImageSlider({images}) {
         autoplaySpeed: 2000
     };
 
+    const optimizeImage = (imageUrl) => {
+        // Example: Resize and compress the image
+        const optimizedUrl = imageUrl + "?w=530&h=400&q=80"; // Adjust dimensions and quality as needed
+        console.log(optimizedUrl)
+        return optimizedUrl;
+    };
+
     return(
         <div className="imgslider">
             <Slider {...settings}>
                 {images.map((item) => {
+                    const optimizedSrc = optimizeImage(item.src);
                     return(
                         <div key={item.id}>
-                            <img src={item.src} alt={item.alt}/>
+                            <img src={optimizedSrc} alt={item.alt} loading="lazy"/>
                         </div>
                     )
                 })}
