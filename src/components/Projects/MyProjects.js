3d1a99d7472c246e3629fb0219f652ad09d9299e
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-13 18:25:56 -0400

    updating code

--- src/components/Projects/MyProjects.js
+++ src/components/Projects/MyProjects.js
@@ -1,60 +1,159 @@
-export const myProject = [{
-  name: 'Gamehub',
-  summary: "Gamehub is a single-page application (SPA) that I conceived and developed entirely on my own. As a personal project aimed at demonstrating proficiency in the MERN stack, Gamehub draws inspiration from platforms like Steam. Leveraging both front-end and back-end technologies, I engineered this platform from the ground up to ensure a smooth and immersive shopping experience for users.",
-  description: "Gamehub is a personal project developed to showcase proficiency in the MERN stack. Inspired by platforms like Steam, I aimed to create a similar experience while adding unique functionalities. As a single-page application (SPA), Gamehub simulates an online store for video games, providing users with a platform to browse, explore, and interact with game listings. Leveraging React for the front-end and Express.js for the back-end, Gamehub implements robust user authentication using JSON Web Tokens (JWT) and ensures secure password hashing with bcrypt. MongoDB serves as the database, facilitating dynamic inventory management through change streams, allowing real-time updates to game availability.\n\nKey features of Gamehub include an interactive game catalog, user account management with distinct privileges for gamers and developers, and responsive cart functionality for seamless browsing and purchasing experiences. The platform prioritizes usability and responsiveness, with an intuitive interface designed to guide users through the browsing and purchasing process effortlessly. Throughout the development process, my biggest struggle and reward was building the search bar and logic from scratch, a challenge that ultimately contributed to the platform's unique functionality and my growth as a developer.\n\nI invite you to explore Gamehub and experience the culmination of my efforts. Building this project has been an immensely rewarding journey, and I'm excited to share it with you. You can visit the Live link for the website or check out the code by clicking on the Code Link.",
-  tech: "HTML, CSS, JavaScript, React, MongoDB, ExpressJS, NodeJS, Git, Heroku",
-  projectLink: 'https://github.com/lucas9121/Gamehub',
-  liveLink: 'https://gamehub-react.herokuapp.com/games',
-  color: '#007bff',
-  images: [],
-},
-{
-  name: "Psych Byte",
-  summary: "Psych Bite is an application that sends an email every morning based off 3 authors revolving around finance, psychology and faith. People can add their email on the website to receive one adn unsubscribe in the link on the email.",
-  description: "",
-  tech: "MongoDB, React, NodeJS, ExpressJS, Nodemailer, Git, Heroku",
-  projectLink: "https://github.com/lucas9121/favorite-quotes",
-  liveLink: "https://psych-bite.herokuapp.com/",
-  color: "green",
-  images: [],
-},
-{
-  name: "Meals To Go",
-  summary: "",
-  description: "",
-  tech: "",
-  projectLink: "",
-  liveLink: "",
-  color: "",
-  images: [],
-},
-{
-  name: "Roaming Spoon",
-  summary: "The Roaming spoon was a group project built in collaboration with a UX team. In the full-stack role, I helped connect the back-end to the front-end and build the search page. Discover amazing food trucks in your area using the Roaming Spoon app! Use The Roaming Spoon to search your favorite food trucks and see their current location. Find out more information for any food truck like location, phone number, business hours, and reviews.",
-  description: "",
-  tech: "HTML, CSS, JavaScript, Google, React, Bootstrap, NodeJS, MongoDB",
-  projectLink: "https://github.com/lucas9121/roaming-food-trucks",
-  liveLink: "https://roaming-spoon.herokuapp.com/",
-  color: "#f2782f",
-  images: [],
-},
-{
-  name: "Wars of Volgrora",
-  summary: "Wars of Vogrora is a RPG turn-style game built using vanila JS. Create a character and join in the fight to save your planet from the forces of evil. Buy potions in the store to increase your attack and defense. Click on the work button to earn money in exchange for energy. Click on the eat button to replenish energy by using money.",
-  description: "",
-  tech: "HTML, CSS, JavaScript, Photoshop",
-  projectLink: "https://github.com/lucas9121/Wars-of-Vogrora",
-  liveLink: "https://lucas9121.github.io/Wars-of-Vogrora/",
-  color: "red",
-  images: [],
-},
-{
-  name: "",
-  summary: "",
-  description: "",
-  tech: "",
-  projectLink: "",
-  liveLink: "",
-  color: "",
-  images: [],
-},]
+
+export const myProject = [
+  {
+    name: 'Gamehub',
+    summary: "Gamehub is a single-page application (SPA) that I conceived and developed entirely on my own. As a personal project aimed at demonstrating proficiency in the MERN stack, Gamehub draws inspiration from platforms like Steam. Leveraging both front-end and back-end technologies, I engineered this platform from the ground up to ensure a smooth and immersive shopping experience for users.",
+    description: "Gamehub is a personal project developed to showcase proficiency in the MERN stack. Inspired by platforms like Steam, I aimed to create a similar experience while adding unique functionalities. As a single-page application (SPA), Gamehub simulates an online store for video games, providing users with a platform to browse, explore, and interact with game listings. Leveraging React for the front-end and Express.js for the back-end, Gamehub implements robust user authentication using JSON Web Tokens (JWT) and ensures secure password hashing with bcrypt. MongoDB serves as the database, facilitating dynamic inventory management through change streams, allowing real-time updates to game availability.\n\nKey features of Gamehub include an interactive game catalog, user account management with distinct privileges for gamers and developers, and responsive cart functionality for seamless browsing and purchasing experiences. The platform prioritizes usability and responsiveness, with an intuitive interface designed to guide users through the browsing and purchasing process effortlessly. Throughout the development process, my biggest struggle and reward was building the search bar and logic from scratch, a challenge that ultimately contributed to the platform's unique functionality and my growth as a developer.\n\nI invite you to explore Gamehub and experience the culmination of my efforts. Building this project has been an immensely rewarding journey, and I'm excited to share it with you. You can visit the Live link for the website or check out the code by clicking on the Code Link.",
+    tech: "HTML, CSS, JavaScript, React, MongoDB, ExpressJS, NodeJS, Git, Heroku",
+    projectIcon: "game",
+    projectLink: 'https://github.com/lucas9121/Gamehub',
+    liveLink: 'https://gamehub-react.herokuapp.com/games',
+    color: '#007bff',
+    images: [
+      {
+        id: 1,
+        src: "/images/GH/GH1.png",
+        alt: "Gamehub image 1"
+      },
+      {
+        id: 2,
+        src: "/images/GH/GH2.png",
+        alt: "Gamehub image 2"
+      },
+      {
+        id: 3,
+        src: "/images/GH/GH3.png",
+        alt: "Gamehub image 3"
+      },
+      {
+        id: 4,
+        src: "/images/GH/GH8.png",
+        alt: "Gamehub image 8"
+      },
+      {
+        id: 5,
+        src: "/images/GH/GH9.png",
+        alt: "Gamehub image 5"
+      }
+    ],
+  },
+  {
+    name: "Psych Byte",
+    summary: "Psych Bite is an application that sends an email every morning based off 3 authors revolving around finance, psychology and faith. People can add their email on the website to receive one adn unsubscribe in the link on the email.",
+    description: "",
+    tech: "MongoDB, React, NodeJS, ExpressJS, Nodemailer, Git, Heroku",
+    projectIcon: "coffee",
+    projectLink: "https://github.com/lucas9121/favorite-quotes",
+    liveLink: "https://psych-bite.herokuapp.com/",
+    color: "green",
+    images: [
+      {
+        id: 1,
+        src: "/images/PB/PB1.png",
+        alt: "Psych bite image 1"
+      },
+      {
+        id: 2,
+        src: "/images/PB/PB2.png",
+        alt: "Psych bite image 2"
+      },
+      {
+        id: 3,
+        src: "/images/PB/PB3.png",
+        alt: "Psych bite image 3"
+      },
+      {
+        id: 4,
+        src: "/images/PB/PB4.png",
+        alt: "Psych bite image 4"
+      }
+  ]
+  },
+  // {
+  //   name: "Meals To Go",
+  //   summary: "",
+  //   description: "",
+  //   tech: "",
+  //   projectIcon: false,
+  //   projectLink: "",
+  //   liveLink: "",
+  //   color: "",
+  //   images: [],
+  // },
+  {
+    name: "Roaming Spoon",
+    summary: "The Roaming spoon was a group project built in collaboration with a UX team. In the full-stack role, I helped connect the back-end to the front-end and build the search page. Discover amazing food trucks in your area using the Roaming Spoon app! Use The Roaming Spoon to search your favorite food trucks and see their current location. Find out more information for any food truck like location, phone number, business hours, and reviews.",
+    description: "",
+    tech: "HTML, CSS, JavaScript, Google, React, Bootstrap, NodeJS, MongoDB",
+    projectIcon: "truck",
+    projectLink: "https://github.com/lucas9121/roaming-food-trucks",
+    liveLink: "https://roaming-spoon.herokuapp.com/",
+    color: "#f2782f",
+    images: [
+      {
+        id: 1,
+        src: "/images/RFT/RFT1.png",
+        alt: "Roaming Spoon image 1"
+      },
+      {
+        id: 2,
+        src: "/images/RFT/RFT2.png",
+        alt: "Roaming Spoon image 2"
+      },
+      {
+        id: 3,
+        src: "/images/RFT/RFT3.png",
+        alt: "Roaming Spoon image 3"
+      },
+      {
+        id: 4,
+        src: "/images/RFT/RFT4.png",
+        alt: "Roaming Spoon image 4"
+      }
+    ],
+  },
+  {
+    name: "Wars of Volgrora",
+    summary: "Wars of Vogrora is a RPG turn-style game built using vanila JS. Create a character and join in the fight to save your planet from the forces of evil. Buy potions in the store to increase your attack and defense. Click on the work button to earn money in exchange for energy. Click on the eat button to replenish energy by using money.",
+    description: "",
+    tech: "HTML, CSS, JavaScript, Photoshop",
+    projectIcon: "door",
+    projectLink: "https://github.com/lucas9121/Wars-of-Vogrora",
+    liveLink: "https://lucas9121.github.io/Wars-of-Vogrora/",
+    color: "red",
+    images: [
+      {
+        id: 1,
+        src: "/images/WOV/WOV1.png",
+        alt: "Wars of Vogrora image 1"
+      },
+      {
+        id: 2,
+        src: "/images/WOV/WOV2.png",
+        alt: "Wars of Vogrora image 2"
+      },
+      {
+        id: 3,
+        src: "/images/WOV/WOV3.png",
+        alt: "Wars of Vogrora image 3"
+      },
+      {
+        id: 4,
+        src: "/images/WOV/WOV4.png",
+        alt: "Wars of Vogrora image 4"
+      },
+    ],
+  },
+  // {
+  //   name: "",
+  //   summary: "",
+  //   description: "",
+  //   tech: "",
+  //   projectIcon: false,
+  //   projectLink: "",
+  //   liveLink: "",
+  //   color: "",
+  //   images: [],
+  // },
+]
+
