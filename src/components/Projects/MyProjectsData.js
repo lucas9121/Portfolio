7378d393eaa75a23b3dd7b2a9be50ba0da029899
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-20 16:31:20 -0400

    chaging data format

---
+++ src/components/Projects/MyProjectsData.js
@@ -0,0 +1,172 @@
+
+const myProjectsData = {
+  "projects": [
+    {
+      "name": 'Gamehub',
+      "summary": "Gamehub is a single-page application (SPA) that I conceived and developed entirely on my own. As a personal project aimed at demonstrating proficiency in the MERN stack, Gamehub draws inspiration from platforms like Steam. Leveraging both front-end and back-end technologies, I engineered this platform from the ground up to ensure a smooth and immersive shopping experience for users.",
+      "description": "Gamehub is a personal project developed to showcase proficiency in the MERN stack. Inspired by platforms like Steam, I aimed to create a similar experience while adding unique functionalities. As a single-page application (SPA), Gamehub simulates an online store for video games, providing users with a platform to browse, explore, and interact with game listings. Leveraging React for the front-end and Express.js for the back-end, Gamehub implements robust user authentication using JSON Web Tokens (JWT) and ensures secure password hashing with bcrypt. MongoDB serves as the database, facilitating dynamic inventory management through change streams, allowing real-time updates to game availability.\n\nKey features of Gamehub include an interactive game catalog, user account management with distinct privileges for gamers and developers, and responsive cart functionality for seamless browsing and purchasing experiences. The platform prioritizes usability and responsiveness, with an intuitive interface designed to guide users through the browsing and purchasing process effortlessly. Throughout the development process, my biggest struggle and reward was building the search bar and logic from scratch, a challenge that ultimately contributed to the platform's unique functionality and my growth as a developer.\n\nI invite you to explore Gamehub and experience the culmination of my efforts. Building this project has been an immensely rewarding journey, and I'm excited to share it with you. You can visit the Live link for the website or check out the code by clicking on the Code Link.",
+      "tech": "HTML, CSS, JavaScript, React, MongoDB, ExpressJS, NodeJS, Heroku, Git",
+      "projectIcon": "game",
+      "projectLink": 'https://github.com/lucas9121/Gamehub',
+      "livetLink": 'https://gamehub-react.herokuapp.com/games',
+      "color": '#007bff',
+      "images": [
+        {
+          id: 1,
+          src: "/images/GH/GH1.png",
+          alt: "Gamehub image 1"
+        },
+        {
+          id: 2,
+          src: "/images/GH/GH2.png",
+          alt: "Gamehub image 2"
+        },
+        {
+          id: 3,
+          src: "/images/GH/GH3.png",
+          alt: "Gamehub image 3"
+        },
+        {
+          id: 4,
+          src: "/images/GH/GH8.png",
+          alt: "Gamehub image 8"
+        },
+        {
+          id: 5,
+          src: "/images/GH/GH9.png",
+          alt: "Gamehub image 5"
+        }
+      ],
+    },
+    {
+      "name": "Psych Byte",
+      "summary": "Psych Byte is a unique application designed to provide users with a daily dose of inspiration and insight from notable authors in the fields of finance, psychology, and faith. Developed in collaboration with another developer, my role as a backend developer focused on building the server-side and subscription management. This service delivers a random quote to subscribers' email inboxes every morning at 7 am. With Psych Byte, users can kickstart their day with a bite of knowledge and motivation.",
+      "description": "Psych Byte emerged from a collaborative effort to deliver daily doses of inspiration through motivational quotes sent directly to users' email inboxes. As the project's backend developer, I focused on building the server-side infrastructure and managing the email subscription system. Leveraging Node.js and Nodemailer, I ensured the smooth delivery of quotes at 7 am each morning, ensuring a timely and consistent user experience. Meanwhile, my teammate took charge of frontend design, crafting an intuitive and visually appealing interface that enhanced user engagement.\n\nCollaborating within a Git and GitHub environment presented initial challenges as we worked separately on our respective ends. Coordinating tasks and integrating backend and frontend components required clear communication and meticulous planning. However, through effective teamwork and a shared commitment to excellence, we overcame these obstacles and delivered a product that surpassed expectations in terms of efficiency and quality.\n\nNow, users can kickstart their day with a bite of knowledge and motivation by subscribing to Psych Byte's daily quotes. Join our community of motivated individuals and experience the power of inspiration firsthand. Visit the live link to explore Psych Byte and subscribe to start your day on a positive note.",
+      "tech": "MongoDB, React, NodeJS, ExpressJS, Nodemailer, Git, Heroku",
+      "projectIcon": "coffee",
+      "projectLink": "https://github.com/lucas9121/favorite-quotes",
+      "livetLink": "https://psych-bite.herokuapp.com/",
+      "color": "green",
+      "images": [
+        {
+          id: 1,
+          src: "/images/PB/PB1.png",
+          alt: "Psych bite image 1"
+        },
+        {
+          id: 2,
+          src: "/images/PB/PB2.png",
+          alt: "Psych bite image 2"
+        },
+        {
+          id: 3,
+          src: "/images/PB/PB3.png",
+          alt: "Psych bite image 3"
+        },
+        {
+          id: 4,
+          src: "/images/PB/PB4.png",
+          alt: "Psych bite image 4"
+        }
+    ]
+    },
+    // {
+    //   "name": "Meals To Go",
+    //   "summary": "Meals To Go is a React Native app developed to test my knowledge of mobile app development using Expo CLI and the Google Maps API. This personal project was inspired by the desire to create a convenient solution for finding restaurants based on the city you search for. Leveraging React Native's capabilities, Meals To Go offers a user-friendly interface and seamless navigation experience.",
+    //   "description": "Meals To Go represents a significant milestone in my journey as a mobile app developer. Developed entirely as a personal project, it serves as a testament to my proficiency in React Native and my ability to integrate third-party APIs seamlessly. The integration of Firebase for user authentication and Google Maps API for restaurant search functionality posed notable challenges, but overcoming these obstacles resulted in a rewarding learning experience.\n\nAlthough there isn't a live version of Meals To Go, you can explore the codebase on GitHub to see how it was built and dive into the details of its functionality. Your feedback and suggestions are always welcome as I continue to refine and enhance this project.",
+    //   "tech": "HTML, CSS, JavaScript, React Native, Expo, Firebase, Git",
+    //   "projectIcon": "",
+    //   "projectLink": "https://github.com/lucas9121/MealsToGo",
+    //   "livetLink": "",
+    //   "color": "#2182BD",
+    //   "images": [],
+    // },
+    {
+      "name": "Roaming Spoon",
+      "summary": "The Roaming spoon was a group project built in collaboration with a UX team. In the full-stack role, I helped connect the back-end to the front-end and build the search page. Discover amazing food trucks in your area using the Roaming Spoon app! Use The Roaming Spoon to search your favorite food trucks and see their current location. Find out more information for any food truck like location, phone number, business hours, and reviews.",
+      "description": "",
+      "tech": "HTML, CSS, JavaScript, Google, React, Bootstrap, NodeJS, MongoDB, Git, Heroku",
+      "projectIcon": "truck",
+      "projectLink": "https://github.com/lucas9121/roaming-food-trucks",
+      "livetLink": "https://roaming-spoon.herokuapp.com/",
+      "color": "#f2782f",
+      "images": [
+        {
+          id: 1,
+          src: "/images/RFT/RFT1.png",
+          alt: "Roaming Spoon image 1"
+        },
+        {
+          id: 2,
+          src: "/images/RFT/RFT2.png",
+          alt: "Roaming Spoon image 2"
+        },
+        {
+          id: 3,
+          src: "/images/RFT/RFT3.png",
+          alt: "Roaming Spoon image 3"
+        },
+        {
+          id: 4,
+          src: "/images/RFT/RFT4.png",
+          alt: "Roaming Spoon image 4"
+        },
+        {
+          id: 5,
+          src: "/images/RFT/RFT5.png",
+          alt: "Roaming Spoon image 5"
+        },
+      ],
+    },
+    {
+      "name": "Wars of Volgrora",
+      "summary": "Wars of Vogrora is a RPG turn-style game built using vanila JS. Create a character and join in the fight to save your planet from the forces of evil. Buy potions in the store to increase your attack and defense. Click on the work button to earn money in exchange for energy. Click on the eat button to replenish energy by using money.",
+      "description": "",
+      "tech": "HTML, CSS, JavaScript, Photoshop",
+      "projectIcon": "door",
+      "projectLink": "https://github.com/lucas9121/Wars-of-Vogrora",
+      "livetLink": "https://lucas9121.github.io/Wars-of-Vogrora/",
+      "color": "red",
+      "images": [
+        {
+          id: 1,
+          src: "/images/WOV/WOV1.png",
+          alt: "Wars of Vogrora image 1"
+        },
+        {
+          id: 2,
+          src: "/images/WOV/WOV2.png",
+          alt: "Wars of Vogrora image 2"
+        },
+        {
+          id: 3,
+          src: "/images/WOV/WOV3.png",
+          alt: "Wars of Vogrora image 3"
+        },
+        {
+          id: 4,
+          src: "/images/WOV/WOV4.png",
+          alt: "Wars of Vogrora image 4"
+        },
+        {
+          id: 5,
+          src: "/images/WOV/WOV5.png",
+          alt: "Wars of Vogrora image 5"
+        },
+      ],
+    },
+    // {
+    //   "name": "",
+    //   "summary": "",
+    //   "description": "",
+    //   "tech": "",
+    //   "projectIcon": "",
+    //   "projectLink": "",
+    //   "livetLink": "",
+    //   "color": "",
+    //   "images": [],
+    // },
+  ]
+}
+
+export default myProjectsData
