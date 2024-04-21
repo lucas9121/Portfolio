f7a03a1d128c3ae256ad6b7c6fb25ce9978d6384
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-20 21:15:19 -0400

    adding functions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
-import { BiMenu } from 'react-icons/bi'
-import "./Header.css"
-import { useState } from 'react'
+import { BiMenu } from 'react-icons/bi';
+import "./Header.css";
+import { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 export default function Headers({toggle, setToggle, sticky, setSticky}) {
     const [show, setShow] = useState(false)
+    const location = useLocation()
+    const navigate = useNavigate()
 
     const handleScrollToTop = () => {
         document.querySelector(".content-section").scrollTo(0, 0)
@@ -17,6 +20,19 @@ export default function Headers({toggle, setToggle, sticky, setSticky}) {
         })
     }
 
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    const isCaseStudyPage = () => {
+        console.log("case study function")
+        console.log(location.pathname.startsWith('/case-study'))
+        return location.pathname.startsWith('/case-study')
+    }
+
     return (
         <header className={sticky ? "sticky" : ""}>
             <div id="portrait" className={!sticky ? 'hide' : 'show'} onClick={() => handleScrollToTop()}></div>
@@ -25,8 +41,14 @@ export default function Headers({toggle, setToggle, sticky, setSticky}) {
             <nav>
                 <BiMenu className='hamburger' onClick={() => {setShow(!show)}} />
                 <ul id={show ? "header-menu" : ""}>
-                    <li className='list'><a href="#About" onClick={() => {setShow(false)}}>About</a></li>
-                    <li className='list'><a href="#Projects" onClick={() => {setShow(false)}}>Projects</a></li>
+                    {isCaseStudyPage() ?
+                        <li className='list'><Link to="/" onClick={() => {setShow(false)}}>Home</Link></li>
+                        :
+                        <>
+                            <li className='list'><Link to="/" onClick={() => {setShow(false); scrollToSection("About")}}>About</Link></li>
+                            <li className='list'><Link to="/" onClick={() => {setShow(false); scrollToSection("Projects")}}>Projects</Link></li>
+                        </>
+                    }
                     <li className='list'><a href="mailto:lucas2carlos@gmail.com" target="_blank" rel="noreferrer" onClick={() => {setShow(false)}}>Contact me</a></li>
                     <li className='list' style={{height: '100%'}}>
                         <div id="toggle">
