64ae24e059564db90a3fea4754bf70a460172714
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-20 21:16:22 -0400

    adding Link and fixing myProjects data import

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Projects.css"
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { BsGithub, BsJoystick } from 'react-icons/bs';
 import { FaCss3Alt, FaBootstrap, FaTruck, FaDungeon, FaCoffee, FaExternalLinkAlt, FaHtml5, FaNodeJs, FaGitAlt } from 'react-icons/fa';
 import { IoLogoJavascript, IoLogoFirebase } from "react-icons/io5";
@@ -8,7 +8,7 @@ import { SiMongodb, SiExpress, SiReact, SiHeroku, SiAdobephotoshop, SiExpo } fro
 import { TbBrandReactNative } from "react-icons/tb";
 import { FcGoogle } from 'react-icons/fc';
 import ImageSlider from '../ImageSlider';
-import { myProject } from './MyProjects'; 
+import myProjectsData from './MyProjectsData'; 
 
 
 const techIcons = {
@@ -42,7 +42,7 @@ const Projects = (props) => {
         <div id="Projects" className={props.toggle ? "project-container dark" : "project-container"}>
             <h2 className={props.toggle ? 'dark-mode' : ''}>My Projects</h2>
             <div className="projects-grid">
-                {myProject.map((project, index) => {
+                {myProjectsData.projects.map((project, index) => {
                     const iconName = project.projectIcon.trim().toLocaleLowerCase()
                     const ExternalIcon = externalLinkIcons[iconName]
                     const iconCssId = iconName === 'none' ? 'external-icon' : project.name.trim().toLocaleLowerCase().replace(/\s/g, '') + "-icon"
@@ -73,9 +73,9 @@ const Projects = (props) => {
                                     })}
                                 </div>
                                 <div className="project-links-container">
-                                    <b>Links</b>
                                     <ul className='project-links'>
                                         {project.projectLink && <li><a className={props.toggle ? "link-dark-mode" : "link"} href={project.projectLink} target="_blank" rel="noreferrer"><BsGithub className="icon github" /></a></li>}
+                                        {project.description && <li><Link className={props.toggle ? "btn" : "btn"} to={`/case-study/${project.name.toLocaleLowerCase()}`}  rel="noreferrer">Case Study</Link></li> }
                                         {project.liveLink && <li><a className={props.toggle ? "link-dark-mode" : "link"} href={project.liveLink} target="_blank" rel="noreferrer"><ExternalIcon id={iconCssId} className={`icon`} /></a></li>}
                                     </ul>
                                 </div>
