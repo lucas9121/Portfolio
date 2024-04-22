80763fd14ebe3eda12b2cec78d40cce05b1c4d1c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2024-04-22 00:02:28 -0400

    adding section div and footer

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,16 @@
 import Projects from "../Projects/Projects";
 import About from "../About/About";
+import Footer from "../Footer/Footer";
 import "./Main.css";
 
-export default function Main({ toggle }) {
+export default function Main({ toggle, cl, isClicked }) {
   return (
-    <main className="Main">
-      <About toggle={toggle} />
-      <Projects toggle={toggle} />
-    </main>
+    <section className="content-section">
+      <main className={!toggle ? "main-light-mode": "main-dark-mode"}>
+        <About toggle={toggle} isClicked={isClicked} />
+        <Projects toggle={toggle} cl={cl} />
+      </main>
+      <Footer toggle={toggle} />
+    </section>
   );
 }
